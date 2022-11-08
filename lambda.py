472e80bdcb187708342d504d334116580387d572
kostyantyn <funyourmind@gmail.com>
2022-11-08 15:32:29 +0100

    modify requirements.txt, .gitignore and refactor lambda.py

--- lambda.py
+++ lambda.py
@@ -1,31 +1,39 @@
-import requests
 import json
+import requests
 import boto3
 
-s3_client = boto3.client('s3')
-
 url = "https://www.arbeitnow.com/api/job-board-api"
-response = requests.get(url)
-raw_data = response.json()
-jobs_data = raw_data["data"]
-job = jobs_data[0]
-job_template = {
-        "id": job['slug'],
-        "title": job['title'],
-        "description": job['description']
-    }
+s3_client = boto3.client('s3')
+bucket = "lambda-arbeitnow-challenge"
 
-for job in jobs_data:
-    job_template = {
-        "id": job['slug'],
-        "title": job['title'],
-        "description": job['description']
-    }
+def get_raw_jobs():    
+    response = requests.get(url)
+    return response.json()["data"]
 
-    job_template_json = json.dumps(job_template)
-      
-    with open("job.txt", "w") as file:
-        file.write(job_template_json)
-        
-    s3_client.upload_file('job.txt', 'lambda-arbeitnow-challenge', job_template['id'])
+def get_jobs_list():
+    raw_jobs = get_raw_jobs()
+    jobs_list = []
+    for job_api in raw_jobs:
+        jobs_list.append({
+            "id": job_api["slug"],
+            "title": job_api["title"],
+            "description": job_api["description"]
+        })
+    return jobs_list
 
+jobs_list = get_jobs_list()
+
+def upload_job(job):
+    job_string = json.dumps(job)
+    job_bytes = str.encode(job_string)
+    s3_client.put_object(
+        Body = job_bytes,
+        Bucket = bucket,
+        Key = job["id"] + ".json"
+    )
+
+def upload_jobs(jobs):
+    for job in jobs:
+        upload_job(job)
+
+upload_jobs(jobs_list)
